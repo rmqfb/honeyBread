ba4e97998ce53025b4d2eacb0acc1b5bf38b47fc
rmqfb <rmqfb@naver.com>
2019-03-02 11:09:30 +0900

    커밋

---
+++ chapter3/C3_2/helloWorld.js
@@ -0,0 +1,22 @@
+/*
+function helloWorld() {
+    console.log('Hello World');
+    helloNode();
+}
+
+function helloNode() {
+    console.log('hello Node');
+}
+
+helloWorld();
+*/
+function helloWorld() {
+    console.log('Hello World');
+    helloNode();
+}
+
+function helloNode() {
+    console.log('hello Node');
+}
+
+helloWorld();
